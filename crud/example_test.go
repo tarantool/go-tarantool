d8df65dcd0f29a6a5d07472115cbcf2753b12609
Albert Skalt <askalt@tarantool.org>
2023-08-21 17:19:36 +0300

    crud: support `operation_data` field in errors
    This patch adds `operation_data` decoding for the `crud.Error`.
    
    The `operation_data` type is determined as `rowType` in `crud.Result`.
    
    Also, according to [1], an error can contain one of the following:
    - an error
    - an array of errors
    - nil
    
    So the error decoding logic has been modified to consider each case,
    in order to avoid comparing an error to nil.
    
    1. https://github.com/tarantool/crud/tree/master#api
    
    Closes #330

--- crud/example_test.go
+++ crud/example_test.go
@@ -78,6 +78,92 @@ func ExampleResult_rowsCustomType() {
 	// [{{} 2010 45 bla}]
 }
 
+// ExampleResult_operationData demonstrates how to obtain information
+// about erroneous objects from crud.Error using `OperationData` field.
+func ExampleResult_operationData() {
+	conn := exampleConnect()
+	req := crud.MakeInsertObjectManyRequest(exampleSpace).Objects([]crud.Object{
+		crud.MapObject{
+			"id":        2,
+			"bucket_id": 3,
+			"name":      "Makar",
+		},
+		crud.MapObject{
+			"id":        2,
+			"bucket_id": 3,
+			"name":      "Vasya",
+		},
+		crud.MapObject{
+			"id":        3,
+			"bucket_id": 5,
+		},
+	})
+
+	ret := crud.Result{}
+	if err := conn.Do(req).GetTyped(&ret); err != nil {
+		crudErrs := err.(crud.ErrorMany)
+		fmt.Println("Erroneous data:")
+		for _, crudErr := range crudErrs.Errors {
+			fmt.Println(crudErr.OperationData)
+		}
+	} else {
+		fmt.Println(ret.Metadata)
+		fmt.Println(ret.Rows)
+	}
+
+	// Output:
+	// Erroneous data:
+	// [2 3 Vasya]
+	// map[bucket_id:5 id:3]
+}
+
+// ExampleResult_operationDataCustomType demonstrates the ability
+// to cast `OperationData` field, extracted from a CRUD error during decoding
+// using crud.Result, to a custom type.
+// The type of `OperationData` is determined as the crud.Result row type.
+func ExampleResult_operationDataCustomType() {
+	conn := exampleConnect()
+	req := crud.MakeInsertObjectManyRequest(exampleSpace).Objects([]crud.Object{
+		crud.MapObject{
+			"id":        1,
+			"bucket_id": 3,
+			"name":      "Makar",
+		},
+		crud.MapObject{
+			"id":        1,
+			"bucket_id": 3,
+			"name":      "Vasya",
+		},
+		crud.MapObject{
+			"id":        3,
+			"bucket_id": 5,
+		},
+	})
+
+	type Tuple struct {
+		Id       uint64 `msgpack:"id,omitempty"`
+		BucketId uint64 `msgpack:"bucket_id,omitempty"`
+		Name     string `msgpack:"name,omitempty"`
+	}
+
+	ret := crud.MakeResult(reflect.TypeOf(Tuple{}))
+	if err := conn.Do(req).GetTyped(&ret); err != nil {
+		crudErrs := err.(crud.ErrorMany)
+		fmt.Println("Erroneous data:")
+		for _, crudErr := range crudErrs.Errors {
+			operationData := crudErr.OperationData.(Tuple)
+			fmt.Println(operationData)
+		}
+	} else {
+		fmt.Println(ret.Metadata)
+		fmt.Println(ret.Rows)
+	}
+	// Output:
+	// Erroneous data:
+	// {1 3 Vasya}
+	// {3 5 }
+}
+
 // ExampleResult_many demonstrates that there is no difference in a
 // response from *ManyRequest.
 func ExampleResult_many() {
