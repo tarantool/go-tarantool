8b2be0133e9d4105491a90dcccd2945dd1e0558d
maxim-konovalov <maksim.konovalov@vk.team>
2024-05-01 12:39:32 +0300

    pool: add missing methods to the pooler interface
    I added methods that are implemented but not included in the pooler interface.

--- pool/pooler.go
+++ pool/pooler.go
@@ -1,15 +1,28 @@
 package pool
 
 import (
+	"context"
 	"time"
 
 	"github.com/tarantool/go-tarantool/v2"
 )
 
+// TopologyEditor is the interface that must be implemented by a connection pool.
+// It describes edit topology methods.
+type TopologyEditor interface {
+	Add(ctx context.Context, instance Instance) error
+	Remove(name string) error
+}
+
 // Pooler is the interface that must be implemented by a connection pool.
 type Pooler interface {
+	TopologyEditor
+
 	ConnectedNow(mode Mode) (bool, error)
 	Close() []error
+	// CloseGraceful closes connections in the ConnectionPool gracefully. It waits
+	// for all requests to complete.
+	CloseGraceful() []error
 	ConfiguredTimeout(mode Mode) (time.Duration, error)
 	NewPrepared(expr string, mode Mode) (*tarantool.Prepared, error)
 	NewStream(mode Mode) (*tarantool.Stream, error)
