6e2c73da324383d766a64e03b750104909b15fd9
Konovalov Maxim <43151027+KaymeKaydex@users.noreply.github.com>
2024-12-05 13:03:16 +0300

    Simplify box creation
    Co-authored-by: Oleg Jukovec <oleg.jukovec@gmail.com>

--- box/box.go
+++ box/box.go
@@ -4,21 +4,15 @@ import (
 	"github.com/tarantool/go-tarantool/v2"
 )
 
-// Box defines an interface for interacting with a Tarantool instance.
-// It includes the Info method, which retrieves instance information.
-type Box interface {
-	Info() (Info, error) // Retrieves detailed information about the Tarantool instance.
-}
-
-// box is a concrete implementation of the Box interface.
+// Box is a helper that wraps for box.* requests.
 // It holds a connection to the Tarantool instance via the Doer interface.
-type box struct {
+type Box struct {
 	conn tarantool.Doer // Connection interface for interacting with Tarantool.
 }
 
 // By returns a new instance of the box structure, which implements the Box interface.
-func By(conn tarantool.Doer) Box {
-	return &box{
+func New(conn tarantool.Doer) Box {
+	return &Box{
 		conn: conn, // Assigns the provided Tarantool connection.
 	}
 }
