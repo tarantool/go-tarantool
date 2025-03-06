39c2ea63f0e3a5d7364aa0b5583f9d55435f15ce
Dmitriy Gertsog <dmyger@gmail.com>
2025-03-06 15:49:02 +0300

    tests: add helpers for TcS
    Simple helpers to make easy create tests required Taranatool
    centralized configuration storage.

---
+++ test_helpers/tcs/prepare.go
@@ -0,0 +1,66 @@
+package tcs
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
+	"text/template"
+	"time"
+
+	"github.com/tarantool/go-tarantool/v2"
+	"github.com/tarantool/go-tarantool/v2/test_helpers"
+)
+
+const (
+	waitTimeout  = 500 * time.Millisecond
+	connectRetry = 3
+	tcsUser      = "client"
+	tcsPassword  = "secret"
+)
+
+//go:embed testdata/config.yaml
+var tcsConfig []byte
+
+func writeConfig(name string, port int) error {
+	cfg, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer cfg.Close()
+
+	cfg.Chmod(0644)
+
+	t := template.Must(template.New("config").Parse(string(tcsConfig)))
+	return t.Execute(cfg, map[string]interface{}{
+		"host": "localhost",
+		"port": port,
+	})
+}
+
+func makeOpts(port int) (test_helpers.StartOpts, error) {
+	opts := test_helpers.StartOpts{}
+	var err error
+	opts.WorkDir, err = os.MkdirTemp("", "tcs_dir")
+	if err != nil {
+		return opts, err
+	}
+
+	opts.ConfigFile = filepath.Join(opts.WorkDir, "config.yaml")
+	err = writeConfig(opts.ConfigFile, port)
+	if err != nil {
+		return opts, fmt.Errorf("can't save file %q: %w", opts.ConfigFile, err)
+	}
+
+	opts.Listen = fmt.Sprintf("localhost:%d", port)
+	opts.WaitStart = waitTimeout
+	opts.ConnectRetry = connectRetry
+	opts.RetryTimeout = waitTimeout
+	opts.InstanceName = "master"
+	opts.Dialer = tarantool.NetDialer{
+		Address:  opts.Listen,
+		User:     tcsUser,
+		Password: tcsPassword,
+	}
+	return opts, nil
+}
